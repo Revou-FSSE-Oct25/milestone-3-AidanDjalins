e0d5586bfc234fa40a03fd2fe33d5d82999cb507
AidanDjalins <aidan.djalins@binus.ac.id>
2026-01-29 11:44:42 +0700

    Completed product card and list design and fetch functionality

---
+++ client/revoshop/types/product.ts
@@ -0,0 +1,20 @@
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+};
+
+export type ProductPlatzi = {
+    id: number;
+    title: string;
+    category: Category;
+    images: string[];
+};
+
+export type Category = {
+  id: number;
+  name: string;
+};
