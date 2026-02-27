ba07f3a365675797e861680267dfc9a481ec5745
AidanDjalins <aidan.djalins@binus.ac.id>
2026-02-27 11:13:37 +0700

    Added proxy.ts layer for route protection

---
+++ client/revoshop/proxy.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function proxy(request:NextRequest) {
+    const token = await getToken({
+        req: request,
+        secret: process.env.NEXTAUTH_SECRET
+    });
+
+    // Protection for admin routes
+    if (request.nextUrl.pathname.startsWith('/admin')) {
+        // (!login) ? redirect to login
+        if (!token) {
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+        
+        // (!admin) ? redirect to home
+        if (token.role !== 'admin') {
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+        
+    }
+     
+    // allow access
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: 'admin/:path*'
+};
